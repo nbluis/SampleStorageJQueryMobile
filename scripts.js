9a7c693847d5565f52b974c207112b0c2b00546b
Marcio Daniel da Rosa <marciodrosa@gmail.com>
2011-11-23 16:18:26 -0200

    Add the geolocation capture.

--- scripts.js
+++ scripts.js
@@ -8,3 +8,21 @@ function load()
 {
 	$("#text").val(localStorage.textValue);
 }
+
+function readGeolocation()
+{
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError);
+	}
+}
+
+function onGeolocationSuccess(position)
+{
+	$("#latitude").val(position.coords.latitude);
+	$("#longitude").val(position.coords.longitude);
+}
+
+function onGeolocationError(message)
+{
+	alert(message);
+}
